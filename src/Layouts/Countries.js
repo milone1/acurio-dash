8abaf938c640f9a86ce44c86694a8ff574c6711e
Erick Milan Marin <erickmilan1002@gmail.com>
2022-06-28 14:44:11 -0500

    Layout contries

---
+++ src/Layouts/Countries.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { Link, Outlet } from "react-router-dom";
+
+const Countries = () => {
+
+  return (
+    <div>
+      <nav className="nav-ecommerce">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png" alt="/" height={100} width={100}/>
+      </nav>
+      <div className="container-tags">
+        <ul className="ul-tags">
+          <li>
+            <a href="/">All</a>
+          </li>
+          <li>
+            <Link to='/ecommerce/arequipa'>Arequipa</Link>
+          </li>
+          <li>
+            <Link to='/ecommerce/barcelona'>Barcelona</Link>
+          </li>
+          <li>
+            <a href="/">Bogota</a>
+          </li>
+          <li>
+            <a href="/">Buenos Aires</a>
+          </li>
+          <li>
+            <a href="/">Chicago</a>
+          </li>
+          <li>
+            <a href="/">Ciudad de Mexico</a>
+          </li>
+          <li>
+            <a href="/">Cuzco</a>
+          </li>
+          <li>
+            <a href="/">Doha</a>
+          </li>
+          <li>
+            <a href="/">Ginebra</a>
+          </li>
+          <li>
+            <a href="/">Lima</a>
+          </li>
+          <li>
+            <a href="/">Miami</a>
+          </li>
+          <li>
+            <a href="/">Paris</a>
+          </li>
+          <li>
+            <a href="/">San Francisco</a>
+          </li>
+          <li>
+            <a href="/">Santiago de Chile</a>
+          </li>
+        </ul>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Countries;
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,21 @@
 import React from "react";
+//* import react router dom
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//* import Login
 import Login from "../pages/Login";
+
+//* import Countries
+import Countries from "../Layouts/Countries";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/countries" element={<Countries />}>
+
+        </Route>
       </Routes>
     </BrowserRouter>
   );
